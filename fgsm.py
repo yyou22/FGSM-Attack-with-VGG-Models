fixed natural accuracy by normalizing the dataset

fgsm.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 #device = torch.device("cuda")
 start_idx = 0
 
-def attack(model, X_data, Y_data):
+def natural(model, X_data, Y_data):
 
 	model.eval()
 
@@ -24,7 +24,6 @@ def attack(model, X_data, Y_data):
 
 		# load original image
 		image = np.array(np.expand_dims(X_data[idx], axis=0), dtype=np.float32)
-		image = np.transpose(image, (0, 3, 1, 2))
 
 		# load label
 		label = np.array([Y_data[idx]], dtype=np.int64)
@@ -49,10 +48,10 @@ def main():
 
 	model = resnet18(pretrained=True)
 
-	X_data = np.load("./data.npy")
-	Y_data = np.load("./label.npy")
+	X_data = np.load("./data_3.npy")
+	Y_data = np.load("./label_3.npy")
 
-	attack(model, X_data, Y_data)
+	natural(model, X_data, Y_data)
 
 if __name__ == "__main__":
 	main()

select_data.py
@@ -7,17 +7,28 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 import numpy as np
 import matplotlib.pyplot as plt
+from torchvision import transforms as T
+from torch.utils.data import DataLoader
 import random
 
 torch.manual_seed(1)
 
+mean = [0.4914, 0.4822, 0.4465]
+std = [0.2471, 0.2435, 0.2616]
+
 def select_data():
 
-	sub_data = []
 	hash_map = {}
 
+	transform_ = T.Compose(
+			[
+				T.ToTensor(),
+				T.Normalize(mean, std),
+			]
+		)
+
 	#retrieve test data
-	test_data_ = datasets.CIFAR10(root='./cifar10', train=False, download=True, transform=transforms.ToTensor())
+	test_data_ = datasets.CIFAR10(root='./cifar10', train=False, download=True, transform=transform_)
 
 	#convert to numpy array
 	test_data = test_data_.data
@@ -56,7 +67,16 @@ def select_data():
 	data = np.array(data)
 	label = np.array(label)
 
-	np.save('./data', data)
+	norm_data = []
+	data = data/255
+
+	for data_ in data:
+		data_ = transform_(data_)
+		norm_data.append(data_.numpy())
+
+	norm_data = np.array(norm_data)
+
+	np.save('./data', norm_data)
 	np.save('./label', label)
 
 def main():